99d8d21dfdf2868e185467af6a85c6c1bc0e845d
sadiqli1 <isaas@code.edu.az>
2022-07-29 15:54:25 +0400

    create product sorting

--- MultiShop/Controllers/ShopController.cs
+++ MultiShop/Controllers/ShopController.cs
@@ -16,27 +16,61 @@ namespace MultiShop.Controllers
         {
             _context = context;
         }
-        public async Task<IActionResult> Index(int? id)
+        public async Task<IActionResult> Index(int? sort, int? id)
         {
-            List<Dress> model = new List<Dress>();
-            if (id == 1)
+            List<Dress> sorts = new List<Dress>();
+            if (sort!=null &&id==null)
             {
-                model = await _context.Dresses.Include(d => d.Images).Include(d => d.Category).Include(d => d.DressInformation).OrderBy(x => x.Name).ToListAsync();
-            }
-            else if (id == 2)
-            {
-                model = await _context.Dresses.Include(d => d.Images).Include(d => d.Category).Include(d => d.DressInformation).OrderByDescending(x => x.Name).ToListAsync();
-            }
-            else if (id == 3)
-            {
-                model = await _context.Dresses.Include(d => d.Images).Include(d => d.Category).Include(d => d.DressInformation).OrderBy(x => x.Price).ToListAsync();
-            }
-            else
-            {
-                model = await _context.Dresses.Include(d => d.Images).Include(d => d.Category).Include(d => d.DressInformation).ToListAsync();
+                switch (sort)
+                {
+                    case 1:
+                        sorts = await _context.Dresses.Include(d => d.Images).Include(d => d.Category).Include(d => d.DressInformation).OrderBy(x => x.Name).ToListAsync();
+                        break;
+                    case 2:
+                        sorts = await _context.Dresses.Include(d => d.Images).Include(d => d.Category).Include(d => d.DressInformation).OrderByDescending(x => x.Name).ToListAsync();
+                        break;
+                    case 3:
+                        sorts = await _context.Dresses.Include(d => d.Images).Include(d => d.Category).Include(d => d.DressInformation).OrderBy(x => x.Price).ToListAsync();
+                        break;
+                    default:
+                        sorts = await _context.Dresses.Include(d => d.Images).Include(d => d.Category).Include(d => d.DressInformation).ToListAsync();
+                        break;
+                }
+                return View(sorts);
             }
 
-            return View(model);
+            if (id != null&&sort==null)
+            {
+                List<Dress> dress = await _context.Dresses.Include(d => d.Images).Include(d => d.Category).Include(d => d.DressInformation).Where(d => d.CategoryId == id).ToListAsync();
+                return View(dress);
+            }
+
+            if (sort != null && id != null)
+            {
+                switch (sort)
+                {
+                    case 1:
+                        sorts = await _context.Dresses.Include(d => d.Images).Include(d => d.Category).Include(d => d.DressInformation).OrderBy(x => x.Name).Where(d => d.CategoryId == id).ToListAsync();
+                        break;
+                    case 2:
+                        sorts = await _context.Dresses.Include(d => d.Images).Include(d => d.Category).Include(d => d.DressInformation).OrderByDescending(x => x.Name).Where(d => d.CategoryId == id).ToListAsync();
+                        break;
+                    case 3:
+                        sorts = await _context.Dresses.Include(d => d.Images).Include(d => d.Category).Include(d => d.DressInformation).OrderBy(x => x.Price).Where(d => d.CategoryId == id).ToListAsync();
+                        break;
+                    default:
+                        sorts = await _context.Dresses.Include(d => d.Images).Include(d => d.Category).Include(d => d.DressInformation).Where(d => d.CategoryId == id).ToListAsync();
+                        break;
+                }
+                return View(sorts);
+            }
+            if(sort == null && id == null)
+            {
+                List<Dress> dress = await _context.Dresses.Include(d => d.Images).Include(d => d.Category).Include(d => d.DressInformation).ToListAsync();
+                return View(dress);
+            }
+
+            return RedirectToAction("Index");
         }
     }
 }
